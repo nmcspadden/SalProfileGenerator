117e5e3ee311fe1419f2d5e07d29445db21314fd
Nick McSpadden <nmcspadden@gmail.com>
2015-01-28 12:48:26 -0800

    Initial commit

---
+++ generate_sal_profile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import argparse
+import os
+from mcxToProfile import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("key", help="Machine Group key")
+parser.add_argument("-u", "--url", help="URL to Sal", default="http://sal")
+args = parser.parse_args()
+
+plistDict = dict()
+
+plistDict['ServerURL'] = args.url
+plistDict['key'] = args.key
+
+newPayload = PayloadDict("com.salsoftware.sal", makeNewUUID(), False, "Sal", "Sal")
+
+newPayload.addPayloadFromPlistContents(plistDict, 'com.salsoftware.sal', 'Always')
+
+newPayload.finalizeAndSave(os.path.join(os.getcwd(), 'com.salsoftware.sal.mobileconfig'))
