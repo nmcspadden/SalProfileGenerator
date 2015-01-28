6f51f3f32bb32e53c702033b8977f79f431c4a8e
Nick McSpadden <nmcspadden@gmail.com>
2015-01-28 15:10:18 -0800

    Changed path to use a default

--- generate_all_profiles.sh
+++ generate_all_profiles.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+default_path="/home/docker/profiles"
+
 oldIFS="$IFS"
 IFS=$'\n'
 results=$( echo "SELECT key FROM server_machinegroup;" | python /home/docker/sal/manage.py dbshell | xargs | awk {'for (i=3; i<NF-1; i++) print $i'} )
 read -rd '' -a lines <<<"$results"
+IFS=$oldIFS
 for line in "${lines[@]}"
 do
 	if [[ -z $1 ]]; then
-		"${BASH_SOURCE[0]}"/generate_sal_profile.py $line
+		$default_path/generate_sal_profile.py $line
 	else
-		"${BASH_SOURCE[0]}"/generate_sal_profile.py $line --url $1
+		$default_path/generate_sal_profile.py $line --url $1
 	fi
 done
