eb17b44a268b418dd8336c8a0807b57e393ba4e0
Graham Gilbert <graham@grahamgilbert.com>
2015-08-26 07:58:56 +0900

    Update to new pref domain

--- generate_sal_profile.py
+++ generate_sal_profile.py
@@ -7,7 +7,7 @@ from mcxToProfile import *
 parser = argparse.ArgumentParser()
 parser.add_argument("key", help="Machine Group key")
 parser.add_argument("-u", "--url", help="Server URL to Sal. Defaults to http://sal.")
-parser.add_argument("-o", "--output", help="Path to output .mobileconfig. Defaults to 'com.salsoftware.sal.mobileconfig' in current working directory.")
+parser.add_argument("-o", "--output", help="Path to output .mobileconfig. Defaults to 'com.github.salopensource.sal.mobileconfig' in current working directory.")
 args = parser.parse_args()
 
 plistDict = dict()
@@ -19,11 +19,11 @@ else:
 
 plistDict['key'] = args.key
 
-newPayload = PayloadDict("com.salsoftware.sal", makeNewUUID(), False, "Sal", "Sal")
+newPayload = PayloadDict("com.github.salopensource.sal", makeNewUUID(), False, "Sal", "Sal")
 
-newPayload.addPayloadFromPlistContents(plistDict, 'com.salsoftware.sal', 'Always')
+newPayload.addPayloadFromPlistContents(plistDict, 'com.github.salopensource.sal', 'Always')
 
-filename = "com.salsoftware.sal"
+filename = "com.github.salopensource.sal"
 
 filename+="." + plistDict['key'][0:5]
 
